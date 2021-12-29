f9848d94cd4a89a9ad26623ea1cf8e8fe7b66a40
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-29 22:52:24 +0100

    LoginPageTestCases Updated

--- src/test/java/testCases/LoginPageTestCases.java
+++ src/test/java/testCases/LoginPageTestCases.java
@@ -5,53 +5,50 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import poms.LoginPagePOM;
 import utils.BaseDriver;
 
 
 public class LoginPageTestCases extends BaseDriver {
 
+    /*
+     * This test checks' login behavior for different usernames.
+     */
     @Test(dataProvider = "credentialsProvider")
     public void loginTests(String username, String password, String expectedBehavior) {
-        WebElement usernameInput = driver.findElement(By.xpath("//input[@id='user-name']"));
-        usernameInput.clear();
-        usernameInput.sendKeys(username);
-        WebElement passwordInput = driver.findElement(By.xpath("//input[@data-test='password']"));
-        passwordInput.clear();
-        passwordInput.sendKeys(password);
-        driver.findElement(By.xpath("//input[contains(@class,'btn_action')]")).click();
+        LoginPagePOM pom = new LoginPagePOM(driver);
+
+        pom.usernameInput.clear();
+        pom.usernameInput.sendKeys(username);
+        pom.passwordInput.clear();
+        pom.passwordInput.sendKeys(password);
+        pom.loginButton.click();
         switch (expectedBehavior){
             case "success":
-                String text = driver.findElement(By.cssSelector("span.title")).getText();
+                String text = pom.titleProducts.getText();
                 Assert.assertEquals(text, "PRODUCTS");
                 driver.navigate().back();
                 break;
             case "error":
-                String errText = driver.findElement(By.xpath("//h3[@data-test=\"error\"]")).getText();
+                String errText = pom.errorMessage.getText();
                 Assert.assertTrue(errText.contains("Sorry"));
-                driver.findElement(By.className("error-button")).click();
+                pom.errorCancel.click();
                 break;
             case "problem":
-                String problemText = driver.findElement(By.cssSelector("#item_4_title_link~.inventory_item_desc")).getText();
+                String problemText = pom.anyTextInProblemPage.getText();
                 Assert.assertTrue(problemText.toLowerCase().contains("sleek"));
                 driver.navigate().back();
                 break;
             case " delay":
-                wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("span.title")));
-                String delayText = driver.findElement(By.cssSelector("span.title")).getText();
+                wait.until(ExpectedConditions.presenceOfElementLocated(pom.titleProductsLocator));
+                String delayText = pom.titleProducts.getText();
                 Assert.assertEquals(delayText, "Products");
                 driver.navigate().back();
                 break;
         }
     }
-    @DataProvider(name = "credentialsProvider")
-    public Object[][] data() {
-        return new Object[][]{
-                {"standard_user", "secret_sauce", "success"},
-                {"locked_out_user", "secret_sauce", "error"},
-                {"problem_user", "secret_sauce", "problem"},
-                {"performance_glitch_user", "secret_sauce", "delay"}
 
-        };
-    }
+
+
 
 }
