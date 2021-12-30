6413b4d09b10f73d2309358ea81b922389c46757
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-30 10:22:15 +0100

    MenuBtnPOM created

---
+++ src/test/java/poms/MenuBtnPOM.java
@@ -0,0 +1,11 @@
+package poms;
+
+import org.openqa.selenium.WebDriver;
+
+public class MenuBtnPOM extends LoginPagePOM{
+    public MenuBtnPOM(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
