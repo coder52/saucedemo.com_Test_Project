5e0e9663c5d84a848547f52453a031bf35959a0c
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-30 11:55:29 +0100

    Update MenuBtnPOM

--- src/test/java/poms/MenuBtnPOM.java
+++ src/test/java/poms/MenuBtnPOM.java
@@ -1,11 +1,27 @@
 package poms;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MenuBtnPOM extends LoginPagePOM{
     public MenuBtnPOM(WebDriver driver) {
         super(driver);
     }
 
+    @FindBy(id = "react-burger-menu-btn")
+    public WebElement menuButton;
+
+    @FindBy(id = "logout_sidebar_link")
+    public WebElement logoutSidebarLink;
+
+    @FindBy(id = "about_sidebar_link")
+    public WebElement aboutSidebarLink;
+
+    @FindBy(id = "shopping_cart_container")
+    public WebElement shoppingCartContainer;
+
+    @FindBy(id = "inventory_sidebar_link")
+    public WebElement inventorySidebarLink;
 
 }
