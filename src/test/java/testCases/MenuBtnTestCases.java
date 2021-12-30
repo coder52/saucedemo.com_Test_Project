dcf2ccc8035f1067f55cc773133d8a20ded7b9f3
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-30 11:55:41 +0100

    Update MenuBtnTestCases

--- src/test/java/testCases/MenuBtnTestCases.java
+++ src/test/java/testCases/MenuBtnTestCases.java
@@ -1,6 +1,5 @@
 package testCases;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -24,8 +23,8 @@ public class MenuBtnTestCases extends BaseDriver {
 
     @Test
     public void logoutTestCase(){
-        waitAndClick(driver.findElement(By.id("react-burger-menu-btn")));
-        waitAndClick(driver.findElement(By.id("logout_sidebar_link")));
+        waitAndClick(pom.menuButton);
+        waitAndClick(pom.logoutSidebarLink);
         String actualLoginValue = pom.loginButton.getAttribute("value").toLowerCase();
         String expectedLoginValue = "login";
         Assert.assertEquals(actualLoginValue, expectedLoginValue);
@@ -34,8 +33,8 @@ public class MenuBtnTestCases extends BaseDriver {
 
     @Test
     public void aboutTestCase(){
-        waitAndClick(driver.findElement(By.id("react-burger-menu-btn")));
-        waitAndClick(driver.findElement(By.id("about_sidebar_link")));
+        waitAndClick(pom.menuButton);
+        waitAndClick(pom.aboutSidebarLink);
         System.out.println(driver.getCurrentUrl());
         String actualUrl = driver.getCurrentUrl();
         String expectedUrl = "https://saucelabs.com/";
@@ -45,9 +44,9 @@ public class MenuBtnTestCases extends BaseDriver {
 
     @Test
     public void inventoryTestCase(){ // All Items test case
-        waitAndClick(driver.findElement(By.id("shopping_cart_container")));
-        waitAndClick(driver.findElement(By.id("react-burger-menu-btn")));
-        waitAndClick(driver.findElement(By.id("inventory_sidebar_link")));
+        waitAndClick(pom.shoppingCartContainer);
+        waitAndClick(pom.menuButton);
+        waitAndClick(pom.inventorySidebarLink);
         wait.until(ExpectedConditions.presenceOfElementLocated(pom.titleProductsLocator));
         String delayText = pom.titleProducts.getText();
         Assert.assertEquals(delayText.toLowerCase(), "products");
