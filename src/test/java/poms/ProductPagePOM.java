ff20c065f4635f031238041fa7e491b85b3146cc
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-31 16:27:58 +0100

    Update POM

--- src/test/java/poms/ProductPagePOM.java
+++ src/test/java/poms/ProductPagePOM.java
@@ -1,10 +1,35 @@
 package poms;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class ProductPagePOM extends MenuBtnPOM{
     public ProductPagePOM(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(className = "product_sort_container")
+    public WebElement productSortContainer;
+
+    @FindBy(className = "inventory_item_name")
+    public List<WebElement> inventoryItemNames;
+
+    @FindBy(className = "inventory_item_price")
+    public List<WebElement> inventoryItemPrices;
+
+    @FindBy(xpath = "//button[text()='Add to cart']")
+    public List<WebElement> addToCartButtons;
+
+    @FindBy(id = "shopping_cart_container")
+    public WebElement shoppingCartContainer;
+
+    @FindBy(xpath = "//button[text()='Remove']")
+    public List<WebElement> removeButtons;
+
+    @FindBy(className = "inventory_details_name")
+    public WebElement inventoryItemNameInDetailsPage;
 
 }
