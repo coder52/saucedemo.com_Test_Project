9fe127ff7e112599e0c26a9009f3896001de9af3
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-31 15:31:00 +0100

    create ProductPagePOM

---
+++ src/test/java/poms/ProductPagePOM.java
@@ -0,0 +1,10 @@
+package poms;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProductPagePOM extends MenuBtnPOM{
+    public ProductPagePOM(WebDriver driver) {
+        super(driver);
+    }
+
+}
