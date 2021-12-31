8e496f764951023030953dea6841d370662cace3
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2021-12-31 15:30:10 +0100

    add isSortedString, isSortedDoubl, getDoubleFromText methods

--- src/test/java/utils/BaseDriver.java
+++ src/test/java/utils/BaseDriver.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class BaseDriver {
     protected WebDriver driver;
     protected WebDriverWait wait;
@@ -23,7 +26,7 @@ public class BaseDriver {
     @AfterClass(alwaysRun = true)
     public void quitDriver(){
 
-        ThreadLocalBaseDriver.quitDriver();
+//        ThreadLocalBaseDriver.quitDriver();
 
     }
 
@@ -51,5 +54,42 @@ public class BaseDriver {
     public String waitAndGetText(WebElement element){
         wait.until(ExpectedConditions.visibilityOf(element));
         return element.getText();
+    }
+
+    public boolean isSortedString(List<String> listOfStrings) {
+        if (listOfStrings.isEmpty() || listOfStrings.size() == 1) {
+            return true;
+        }
+
+        Iterator<String> iter = listOfStrings.iterator();
+        String current, previous = iter.next();
+        while (iter.hasNext()) {
+            current = iter.next();
+            if (previous.compareTo(current) > 0) {
+                return false;
+            }
+            previous = current;
+        }
+        return true;
+    }
+    public boolean isSortedDouble(List<Double> listOfStrings) {
+        if (listOfStrings.isEmpty() || listOfStrings.size() == 1) {
+            return true;
+        }
+
+        Iterator<Double> iter = listOfStrings.iterator();
+        double current, previous = iter.next();
+        while (iter.hasNext()) {
+            current = iter.next();
+            if (previous > current) {
+                return false;
+            }
+            previous = current;
+        }
+        return true;
+    }
+
+    public Double getDoubleFromText(String cartPriceText) {
+        return Double.valueOf(cartPriceText.replaceAll( "[^\\d.]","" ));
     }
 }
