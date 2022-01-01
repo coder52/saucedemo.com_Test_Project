206050fe9e35dac190f85247b552e3200f9a61b6
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2022-01-01 11:51:10 +0100

    update CartPagePOM

---
+++ src/test/java/poms/CartPagePOM.java
@@ -0,0 +1,40 @@
+package poms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CartPagePOM extends DetailsPagePOM{
+    public CartPagePOM(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "continue-shopping")
+    public WebElement continueShoppingButton;
+
+    @FindBy(id = "checkout")
+    public WebElement checkoutbutton;
+
+    @FindBy(id = "header_container")
+    public WebElement headerContainer;
+
+    @FindBy(id = "cancel")
+    public WebElement cancelButton;
+
+    @FindBy(className = "cart_desc_label")
+    public WebElement cartDescriptionLabel;
+
+    @FindBy(id = "first-name")
+    public WebElement firstNameBox;
+
+    @FindBy(id = "last-name")
+    public WebElement lastNameBox;
+
+    @FindBy(id = "postal-code")
+    public WebElement postalCodeBox;
+
+    @FindBy(id = "continue")
+    public WebElement continueButton;
+
+
+}
